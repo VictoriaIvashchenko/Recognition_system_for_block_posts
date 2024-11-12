be545d654fad2c25d2b0805fd952c90d4b4692f4
Vika <vsvashchenko@gmail.com>
2024-11-12 10:51:05 +0200

    виправлення розпізнавання автомобільних номерів. Налаштування багатопотоковості.

--- car_recognition.py
+++ car_recognition.py
@@ -1,8 +1,6 @@
 import cv2
-import torch
 from ultralytics import YOLO
-import config
-import time
+import parameters
 
 
 def detect_cars_from_video(cap):
@@ -23,21 +21,9 @@ def detect_cars_from_video(cap):
             class_id = int(result.cls)
             if class_id == 2:  # Індекс класу для автомобілів у COCO — 2
                 car_detected = True
-                x1, y1, x2, y2 = map(int, result.xyxy[0])
-                # Малюємо рамку
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(frame, "Car", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
         # Оновлюємо глобальну змінну
-        config.global_var = car_detected
-
-        # Відображаємо кадр з детекцією
-        cv2.imshow("Car Detection", frame)
-
-        # Зупиняємо, якщо натиснута клавіша 'q'
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        parameters.global_var = car_detected
 
     cap.release()
-    cv2.destroyAllWindows()
 
--- video_record.py
+++ video_record.py
@@ -1,42 +1,85 @@
 import time
 import cv2
-import config
+import pytesseract
+import numpy as np
+import re
+import parameters
+
+# Вказуємо шлях до виконуваного файлу Tesseract (для Windows)
+pytesseract.pytesseract.tesseract_cmd = r'D:\Python\work\tools\tesseract.exe'
+
+# Регулярний вираз для українських номерів
+ukraine_plate_pattern = r"^[ABCEHIKMOPTX01]{2}\s?\d{4}\s?[ABCEHIKMOPTX01]{2}$"
 
 
 def record_video(cap):
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = None
+    plate_number = None
+    video_name = None
+    stop_time = None  # Час, коли потрібно завершити запис після зміни is_recording на False
 
     while True:
-        is_recording = config.global_var
+        is_recording = parameters.global_var
         ret, frame = cap.read()
         if not ret:
             break
 
-        # Записуємо кадр, якщо запис увімкнено
+        # Якщо почався запис, ініціалізуємо об'єкт запису
         if is_recording:
             if out is None:
                 video_name = "car_" + time.strftime("%Y%m%d-%H%M") + ".avi"
-                # Ініціалізуємо об'єкт запису відео при першому запуску
                 out = cv2.VideoWriter(video_name, fourcc, 20.0, (frame.shape[1], frame.shape[0]))
-                print("Start recording.")
+                # print("Start recording.")
+            stop_time = None  # Скидаємо таймер завершення запису, якщо recording знову увімкнений
+
             out.write(frame)
+
+            # Визначаємо номерний знак, якщо його ще не розпізнано
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+            edged = cv2.Canny(blurred, 50, 200)
+            contours, _ = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+
+            license_plate = None
+
+            for contour in contours:
+                perimeter = cv2.arcLength(contour, True)
+                approx = cv2.approxPolyDP(contour, 0.018 * perimeter, True)
+                if len(approx) == 4:
+                    license_plate = approx
+                    break
+
+            if license_plate is not None:
+                mask = np.zeros(gray.shape, dtype=np.uint8)
+                cv2.drawContours(mask, [license_plate], -1, 255, -1)
+                masked_image = cv2.bitwise_and(gray, gray, mask=mask)
+                (x, y, w, h) = cv2.boundingRect(license_plate)
+                cropped = masked_image[y:y + h, x:x + w]
+                config = "--psm 8 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+                plate_number = pytesseract.image_to_string(cropped, config=config).strip()
+
+                if bool(re.match(ukraine_plate_pattern, plate_number)):
+                    print("License plate:", plate_number)
+
         else:
-            # Зупиняємо запис, якщо він був увімкнений
-            if out is not None:
-                time.sleep(15)
+            # Запускаємо таймер на 10 секунд при відключенні запису
+            if stop_time is None:
+                stop_time = time.time() + 10
+
+            # Якщо 10 секунд пройшло після вимкнення запису, зупиняємо запис
+            if (time.time() > stop_time) and is_recording is False:
                 out.release()
                 out = None
-                print("Finish recording.")
-
-        # Завершення роботи при натисканні клавіші 'q'
-        if cv2.waitKey(1) == ord('q'):
-            break
+                # print("Finish recording.")
+                stop_time = None  # Скидаємо таймер завершення запису
 
     # Завершуємо роботу з камерою та звільняємо ресурси
     cap.release()
     if out is not None:
         out.release()
     cv2.destroyAllWindows()
+
 
 
